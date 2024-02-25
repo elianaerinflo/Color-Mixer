52e3199bafc3aaf53b6c5432f5842edfeaa8c93c
Eleana Erin Flores <160589391+elianaerinflo@users.noreply.github.com>
2024-02-25 20:55:17 +0800

    Add files via upload

---
+++ Color Mixer/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(39, 118)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(196, 177)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.Location = New System.Drawing.Point(291, 118)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(215, 140)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox3.Location = New System.Drawing.Point(582, 118)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(225, 177)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.Label1.Location = New System.Drawing.Point(36, 85)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "COLOR 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.Label2.Location = New System.Drawing.Point(288, 85)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(157, 18)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "COLOR MIXER RESULT"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.Label3.Location = New System.Drawing.Point(579, 85)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(66, 18)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "COLOR 2"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.ForeColor = System.Drawing.Color.Maroon
+        Me.Button1.Location = New System.Drawing.Point(308, 266)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(186, 48)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "MIX"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"RED", "BLUE", "GREEN", "NOTHING"})
+        Me.ComboBox1.Location = New System.Drawing.Point(81, 302)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 26)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"RED", "BLUE", "GREEN", "NOTHING"})
+        Me.ComboBox2.Location = New System.Drawing.Point(649, 302)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 26)
+        Me.ComboBox2.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Bisque
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(843, 438)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Palatino Linotype", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "COLOR MIXER"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+End Class
---
+++ Color Mixer/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedItem = "RED" Then
+            Label1.Text = ("RED")
+            Label1.ForeColor = Color.Red
+            PictureBox1.BackColor = Color.Red
+        ElseIf ComboBox1.SelectedItem = "GREEN" Then
+            Label1.Text = ("GREEN")
+            Label1.ForeColor = Color.Green
+            PictureBox1.BackColor = Color.Green
+        ElseIf ComboBox1.SelectedItem = "BLUE" Then
+            Label1.Text = ("RED")
+            Label1.ForeColor = Color.Blue
+            PictureBox1.BackColor = Color.Blue
+        ElseIf ComboBox1.SelectedItem = "NOTHING" Then
+            Label1.Text = ("NOTHING")
+            Label1.ForeColor = Color.SaddleBrown
+            PictureBox1.BackColor = Color.Transparent
+        End If
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        If ComboBox2.SelectedItem = "RED" Then
+            Label3.Text = ("RED")
+            Label3.ForeColor = Color.Red
+            PictureBox3.BackColor = Color.Red
+        ElseIf ComboBox2.SelectedItem = "GREEN" Then
+            Label3.Text = ("GREEN")
+            Label3.ForeColor = Color.Green
+            PictureBox3.BackColor = Color.Green
+        ElseIf ComboBox2.SelectedItem = "BLUE" Then
+            Label3.Text = ("BLUE")
+            Label3.ForeColor = Color.Blue
+            PictureBox3.BackColor = Color.Blue
+        ElseIf ComboBox2.SelectedItem = "NOTHING" Then
+            Label3.Text = ("NOTHING")
+            Label3.ForeColor = Color.SaddleBrown
+            PictureBox3.BackColor = Color.Transparent
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (ComboBox1.Text = "RED" And ComboBox2.Text = "BLUE") Or
+           (ComboBox1.Text = "BLUE" And ComboBox2.Text = "RED") Then
+            PictureBox2.BackColor = Color.Magenta
+        ElseIf (ComboBox1.Text = "BLUE" And ComboBox2.Text = "GREEN") Or
+                (ComboBox1.Text = "GREEN" And ComboBox2.Text = "BLUE") Then
+            PictureBox2.BackColor = Color.Turquoise
+        ElseIf (ComboBox1.Text = "GREEN" And ComboBox2.Text = "RED") Or
+                (ComboBox1.Text = "RED" And ComboBox2.Text = "GREEN") Then
+            PictureBox2.BackColor = Color.Yellow
+        ElseIf (ComboBox1.Text = "RED" And ComboBox2.Text = "RED") Then
+            PictureBox2.BackColor = Color.Red
+        ElseIf (ComboBox1.Text = "GREEN" And ComboBox2.Text = "GREEN") Then
+            PictureBox2.BackColor = Color.Green
+        ElseIf (ComboBox1.Text = "BLUE" And ComboBox2.Text = "BLUE") Then
+            PictureBox2.BackColor = Color.Blue
+        ElseIf (ComboBox1.Text = "GREEN" And ComboBox2.SelectedItem = "NOTHING") Or
+                (ComboBox1.SelectedItem = "NOTHING" And ComboBox2.Text = "GREEN") Then
+            PictureBox2.BackColor = Color.Green
+        ElseIf (ComboBox1.Text = "RED" And ComboBox2.SelectedItem = "NOTHING") Or
+                (ComboBox1.SelectedItem = "NOTHING" And ComboBox2.Text = "RED") Then
+            PictureBox2.BackColor = Color.Red
+        ElseIf (ComboBox1.Text = "BLUE" And ComboBox2.SelectedItem = "NOTHING") Or
+                (ComboBox1.SelectedItem = "NOTHING" And ComboBox2.Text = "BLUE") Then
+            PictureBox2.BackColor = Color.Blue
+        ElseIf (ComboBox1.SelectedItem = "NOTHING" And ComboBox2.SelectedItem = "NOTHING") Then
+            PictureBox2.BackColor = Color.Transparent
+        End If
+    End Sub
+End Class
